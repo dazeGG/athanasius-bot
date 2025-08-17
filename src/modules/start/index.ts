ad7b5e40556c4fce892cfb00b2f799c51d8c4e49
Vladimir <86754494+dazeGG@users.noreply.github.com>
2025-08-17 23:14:23 +0900

    Revert "feat: created initial start module (#1)" (#2)
    This reverts commit 51e50fde457b19ebc6ff8db3b6d4fb580295053b.

--- src/modules/start/index.ts
+++
@@ -1,9 +0,0 @@
-import { BOT } from '~/core';
-
-import * as handlers from './handlers';
-
-const registerStart = () => {
-	BOT.registerHandler('/start', handlers.start);
-};
-
-export default registerStart;
