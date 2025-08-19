7e33c119e93769c357164310b68448fda41f9dae
VladimirBelyaev <vogisa@mail.ru>
2025-08-19 23:42:56 +0900

    chore: added cards and games to schema

--- src/core/entities/db/index.ts
+++ src/core/entities/db/index.ts
@@ -1,13 +1,19 @@
 import { JSONFilePreset } from 'lowdb/node';
+// import { nanoid } from 'nanoid';
 
-import type { User } from './types';
+import type { Card, Game, User } from './types';
 
 type DatabaseSchema = {
+	cards: Card[];
 	users: User[];
+	games: Game[];
 };
 
 const DB = await JSONFilePreset<DatabaseSchema>('db.json', {
+	// cards: ['Hearts', 'Diamonds', 'Clubs', 'Spades'].map(suit => ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'].map(name => ({ id: nanoid(), name, suit }))),
+	cards: [],
 	users: [],
+	games: [],
 });
 
 export default DB;
--- src/core/entities/db/types.ts
+++ src/core/entities/db/types.ts
@@ -1,7 +1,21 @@
 import type TelegramBot from 'node-telegram-bot-api';
 
+export type Card = {
+	id: string;
+	name: string;
+	value: number;
+	suit: string;
+};
+
 export type User = {
 	id: TelegramBot.User['id'];
 	username: TelegramBot.User['username'];
 	name: string;
-}
+};
+
+export type Game = {
+	id: string;
+	queue: number[];
+	decks: Record<number, string[]>;
+	athanasiuses: Record<number, string[]>;
+};
