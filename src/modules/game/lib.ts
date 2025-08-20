e33a2c1cc549a98c9a5cd36fd71b52e4f202c69f
VladimirBelyaev <vogisa@mail.ru>
2025-08-20 14:51:33 +0900

    fix: texts

--- src/modules/game/lib.ts
+++ src/modules/game/lib.ts
@@ -3,7 +3,7 @@ export const txt = {
 	notStarted: '<b>Игра ещё не начата</b>',
 	ongoing: '<b>Игра уже идет</b>',
 	players: 'Список игроков',
-	gameSettings: 'Вот настройки игры',
+	gameSettings: 'Настройки игры',
 	decksCount: 'Количество колод',
 	playersCountError: '<b>Чтобы начать игру, необходимо как минимум 3 игрока!</b>',
 } as const;
