3ed8a7ff4a1d0576a2f2dfeee83f3a8a72abbd81
VladimirBelyaev <vogisa@mail.ru>
2025-08-20 15:37:32 +0900

    feat: added auth guard to settings module

--- src/modules/settings/index.ts
+++ src/modules/settings/index.ts
@@ -1,12 +1,13 @@
 import { BOT } from '~/core';
+import { isRegistered } from '~/lib';
 
 import * as handlers from './handlers';
 
 const registerSettings = () => {
-	BOT.registerMessageHandler(handlers.settingsStartMessageHandler, { exact: 'Настройки' });
-	BOT.registerMessageHandler(handlers.settingsChangeNameStateMessageHandler, { state: 'SETTINGS_CHANGE_NAME' });
+	BOT.registerMessageHandler(handlers.settingsStartMessageHandler, { exact: 'Настройки' }, isRegistered);
+	BOT.registerMessageHandler(handlers.settingsChangeNameStateMessageHandler, { state: 'SETTINGS_CHANGE_NAME' }, isRegistered);
 
-	BOT.registerCallbackHandler(handlers.settingsChangeNameCallbackHandler, { module: 'settings', action: 'changeName' });
+	BOT.registerCallbackHandler(handlers.settingsChangeNameCallbackHandler, { module: 'settings', action: 'changeName' }, isRegistered);
 };
 
 export default registerSettings;
