c748d4551b5762b57adfa56e7a1411560454bde8
VladimirBelyaev <vogisa@mail.ru>
2025-08-20 14:43:39 +0900

    feat: worked settings module

--- src/modules/settings/handlers.ts
+++ src/modules/settings/handlers.ts
@@ -1,8 +1,53 @@
-import { BOT, DB } from '~/core';
-import type { MessageContext } from '~/core';
+import { BOT, DB, STATES } from '~/core';
+import { validateName } from '~/lib';
+import type { MessageContext, CallbackContext } from '~/core';
 
 import * as lib from './lib';
 
+const sendBaseSettingsMessage = async (ctx: MessageContext) => {
+	const user = DB.data.users.find(user => user.id === ctx.message.from.id);
+
+	const settingsStartMessage = '<b>' + lib.txt.yourSettings + ':</b>\n' +
+		'\n' +
+		'• ' + lib.txt.name + ': ' + user?.name + '\n' +
+		'\n' +
+		lib.txt.chooseWhatToChange;
+
+	await BOT.sendMessage({ ctx, message: settingsStartMessage, keyboard: lib.kb.baseSettings });
+};
+
 export const settingsStartMessageHandler = async (ctx: MessageContext) => {
-	await BOT.sendMessage({ ctx, message: lib.txt.start });
+	await sendBaseSettingsMessage(ctx);
+};
+
+export const settingsChangeNameCallbackHandler = async (ctx: CallbackContext) => {
+	await BOT.answerCallbackQuery(ctx);
+	await BOT.editMessage({ ctx, message: lib.txt.changeName });
+
+	STATES.setState(ctx.callback.from.id, 'SETTINGS_CHANGE_NAME');
+};
+
+export const settingsChangeNameStateMessageHandler = async (ctx: MessageContext) => {
+	const newName = ctx.message.text;
+
+	const validationData = validateName(newName);
+
+	if (!validationData.success) {
+		await BOT.sendMessage({ ctx, message: '<b>Ошибка!</b>\n\n' + validationData.message });
+	} else {
+		await DB.update(({ users }) => {
+			const user = DB.data.users.find(user => user.id === ctx.message.from.id);
+
+			if (user) {
+				user.name = newName;
+			}
+
+			return { users };
+		});
+
+		await BOT.sendMessage({ ctx, message: lib.txt.success });
+		await sendBaseSettingsMessage(ctx);
+
+		STATES.clearState(ctx.message.from.id);
+	}
 };
