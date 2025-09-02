41074fc6fcadca22e445e3abb6c5b92dd3626efd
VladimirBelyaev <vogisa@mail.ru>
2025-09-02 18:52:38 +0900

    refactor: me info messages

--- src/modules/game/ui/message/info-message.ts
+++ src/modules/game/ui/message/info-message.ts
@@ -62,34 +62,40 @@ export class InfoMessage {
 	}
 
 	/* ME */
-	private static meWrong (turnMeta: TurnMeta): string {
-		return '<b>К сожалению, ты не угадал</b>\n' +
-			'\n' +
-			'У ' + turnMeta.player.name;
+	private static get meWrongBase (): string {
+		return '<b>К сожалению, ты не угадал :(</b>\n\n';
+	}
+
+	private static meWrongWithCard (turnMeta: CountStageMeta | ColorsStageMeta | SuitsStageMeta): string {
+		return this.meWrongBase + `Карта: ${CARDS_VIEW_MAP[turnMeta.cardName]}\n`;
+	}
+
+	private static meWrongWithCount (turnMeta: ColorsStageMeta | SuitsStageMeta): string {
+		return this.meWrongWithCard(turnMeta) + `Количество: <b>${turnMeta.count}</b>\n`;
 	}
 
 	public static wrongCardMe (ctx: CallbackContext, turnMeta: CardStageMeta): EditMessageOptions {
-		return { ctx, text: this.meWrong(turnMeta) + ` нет ${CARDS_VIEW_MAP[turnMeta.cardName]} :(` };
+		return { ctx, text: this.meWrongBase + `У ${turnMeta.player.name} нет ${CARDS_VIEW_MAP[turnMeta.cardName]}` };
 	}
 
 	public static wrongCountMe (ctx: CallbackContext, turnMeta: CountStageMeta): EditMessageOptions {
 		return {
 			ctx,
-			text: this.meWrong(turnMeta) + ` количество ${CARDS_VIEW_MAP[turnMeta.cardName]} не ${turnMeta.count} :(`,
+			text: this.meWrongWithCard(turnMeta) + `Не ${turnMeta.count}`,
 		};
 	}
 
 	public static wrongColorsMe (ctx: CallbackContext, turnMeta: ColorsStageMeta): EditMessageOptions {
 		return {
 			ctx,
-			text: this.meWrong(turnMeta) + ` количество красных ${CARDS_VIEW_MAP[turnMeta.cardName]} не ${turnMeta.redCount} :(`,
+			text: this.meWrongWithCount(turnMeta) + `Не 🔴: ${turnMeta.redCount} ⚫: ${turnMeta.blackCount}`,
 		};
 	}
 
 	public static wrongSuitsMe (ctx: CallbackContext, turnMeta: SuitsStageMeta): EditMessageOptions {
 		return {
 			ctx,
-			text: this.meWrong(turnMeta) + ` масти не ♥️: ${turnMeta.suits.hearts} ♦️: ${turnMeta.suits.diamonds} ♠️: ${turnMeta.suits.spades} ♣️: ${turnMeta.suits.clubs} :(`,
+			text: this.meWrongWithCount(turnMeta) + `Не ♥️: ${turnMeta.suits.hearts} ♦️: ${turnMeta.suits.diamonds} ♠️: ${turnMeta.suits.spades} ♣️: ${turnMeta.suits.clubs}`,
 		};
 	}
 
