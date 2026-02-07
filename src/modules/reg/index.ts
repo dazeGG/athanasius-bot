9b100663e5f63e98f49833efe4eeb86c31c97bd9
VladimirEfremov <vogisa@mail.ru>
2026-02-07 12:30:53 +0300

    refactor: some registration flow

--- src/modules/reg/index.ts
+++ src/modules/reg/index.ts
@@ -3,7 +3,8 @@ import { BOT } from '~/core';
 import * as handlers from './handlers';
 
 const registerReg = () => {
-	BOT.registerMessageHandler(handlers.regStartMessageHandler, { startsWith: '/reg ' });
+	BOT.registerCommand('/reg', handlers.regStartMessageHandler);
+	BOT.registerMessageHandler(handlers.regNameStateMessageHandler, { state: 'REGISTRATION' });
 };
 
 export default registerReg;
