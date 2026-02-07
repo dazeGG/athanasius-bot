9ba17cf4044ce71baa9f6ab2afdfdb263a36b072
Vladimir <86754494+dazeGG@users.noreply.github.com>
2026-02-08 01:45:40 +0300

    feature/new-player-notification (#22)
    * chore: added --fix to lint script
    
    * feat: added new player notification

--- src/modules/reg/handlers.ts
+++ src/modules/reg/handlers.ts
@@ -46,4 +46,13 @@ export const regNameStateMessageHandler = async (ctx: MessageContext) => {
 
 	await BOT.sendMessage({ ctx, text: lib.txt.successfulRegistration });
 	STATES.clearState(ctx.message.from.id);
+
+	const usersExceptMe = DB.data.users.filter(u => u.id !== user.id);
+
+	for (const u of usersExceptMe) {
+		await BOT.sendMessageByChatId({
+			chatId: u.id,
+			text: `У нас новый игрок - ${name}`,
+		});
+	}
 };
