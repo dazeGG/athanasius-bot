4441c086d5c368c286b13e880dfea98ffec02a19
VladimirEfremov <vogisa@mail.ru>
2026-02-08 11:58:42 +0300

    feature: added updates composed view notify

--- src/modules/game/services/game-notifications.service.ts
+++ src/modules/game/services/game-notifications.service.ts
@@ -9,9 +9,24 @@ import type { GameServiceOptions, GameServiceOptionsStage, UpdateMessageOptionsS
 
 export class GameNotificationsService {
 	public static async sendFirstMessage (game: Game, initial: boolean = false) {
+		let text = '';
+
+		if (initial) {
+			text = txt.firstTurnMessage;
+		} else {
+			text = '<b>Твой ход!</b>\n\nВыбери у кого хочешь спросить карту';
+
+			if (game.activePlayer.settings.updatesView === 'composed') {
+				await BOT.sendMessageByChatId({
+					chatId: game.activePlayer.id,
+					text: `🟨 Вот что было за последний круг:\n\n${game.getLastRoundLogs()}`,
+				});
+			}
+		}
+
 		await BOT.sendMessageByChatId({
 			chatId: game.activePlayer.id,
-			text: initial ? txt.firstTurnMessage : '<b>Твой ход!</b>\n\nВыбери у кого хочешь спросить карту',
+			text,
 			keyboard: gkb.playersSelect(game.activePlayer.id, game.gameId, game.allPlayers),
 		});
 	}
