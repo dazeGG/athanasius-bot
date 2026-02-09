901e2c56a153e0868efbd95299bb795b662c1ef9
Vladimir <86754494+dazeGG@users.noreply.github.com>
2026-02-09 14:26:28 +0300

    fix: send hand message -> edit | added update button to hand (#30)

--- src/modules/hand/handlers.ts
+++ src/modules/hand/handlers.ts
@@ -4,7 +4,7 @@ import type TelegramBot from 'node-telegram-bot-api';
 import { ORM } from '~/db';
 import { Deck } from '~/entities/deck';
 import { Game } from '~/entities/game';
-import type { MessageContext, CallbackContext } from '~/core';
+import type { MessageContext, CallbackContext , EditMessageOptions } from '~/core';
 
 import * as ui from './ui';
 
@@ -14,17 +14,11 @@ const getHandsOptions = (me: TelegramBot.User) => {
 	if (gamesWithMe.length === 0) {
 		return { text: ui.txt.noActiveGames };
 	} else {
-		return { text: ui.txt.chooseGame, keyboard: ui.gkb.roomsList(gamesWithMe) };
+		return { text: ui.txt.chooseGame, keyboard: ui.gkb.gamesList(gamesWithMe) };
 	}
 };
 
-export const handMessageHandler = async (ctx: MessageContext) => {
-	await BOT.deleteMessage(ctx);
-	await BOT.sendMessage({ ctx, ...getHandsOptions(ctx.message.from) });
-};
-
-export const handShowCallbackHandler = async (ctx: CallbackContext) => {
-	await BOT.answerCallbackQuery(ctx);
+const getHandShowOptions = (ctx: CallbackContext): EditMessageOptions => {
 	const { from: me, data: { meta: gameId } } = ctx.callback;
 
 	if (!gameId) {
@@ -34,16 +28,26 @@ export const handShowCallbackHandler = async (ctx: CallbackContext) => {
 	const game = gameId ? new Game({ id: gameId }) : null;
 
 	if (!game) {
-		return;
+		throw new Error('Game not found');
 	}
 
 	const hand = game.getHand(me.id);
 
 	if (!hand) {
-		return;
+		throw new Error('Hand not found');
 	}
 
-	await BOT.sendMessage({ ctx, text: Deck.getMyHandView(hand.cardsInHand), keyboard: ui.kb.handBack });
+	return { ctx, text: Deck.getMyHandView(hand.cardsInHand), keyboard: ui.gkb.hand(game) };
+};
+
+export const handMessageHandler = async (ctx: MessageContext) => {
+	await BOT.deleteMessage(ctx);
+	await BOT.sendMessage({ ctx, ...getHandsOptions(ctx.message.from) });
+};
+
+export const handShowCallbackHandler = async (ctx: CallbackContext) => {
+	await BOT.answerCallbackQuery(ctx);
+	await BOT.editMessage(getHandShowOptions(ctx));
 };
 
 export const handCloseCallbackHandler = async (ctx: CallbackContext) => {
