36a1dca791284b58ec6ba6ac8f5546b4a97128f4
VladimirEfremov <vogisa@mail.ru>
2026-02-09 13:07:05 +0300

    fix: typing

--- src/shared/ui/game/athanasiuses-list.ts
+++ src/shared/ui/game/athanasiuses-list.ts
@@ -1,5 +1,6 @@
 import { ORM } from '~/db';
 import { DeckConfig } from '~/entities/deck';
+import type { CardName } from '~/entities/deck';
 import type { Game } from '~/entities/game';
 
 export const athanasiusesList = (game: Game): string => {
@@ -7,7 +8,7 @@ export const athanasiusesList = (game: Game): string => {
 		.allPlayers
 		.map(playerId => {
 			const player = ORM.Users.get(playerId);
-			return '• ' + player.name + ': ' + game.getAthanasiuses()[playerId].map(athanasius => DeckConfig.CARDS_VIEW_MAP[athanasius]).join(' ');
+			return '• ' + player.name + ': ' + game.getAthanasiuses()[playerId].map(athanasius => DeckConfig.CARDS_VIEW_MAP[athanasius as CardName]).join(' ');
 		})
 		.join('\n');
 };
