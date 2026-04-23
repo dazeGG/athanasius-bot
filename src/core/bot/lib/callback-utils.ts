e352020e065367d06fc465c8bddcd7bd827c1387
VladimirBelyaev <dazevvv@gmail.com>
2026-04-23 20:53:54 +0900

    refactor(core): refactor callback utilities - add stringifyCallbackData, remove composeCallbackData

--- src/core/bot/lib/callback-utils.ts
+++ src/core/bot/lib/callback-utils.ts
@@ -1,21 +1,16 @@
-import type { CallbackData } from '..';
-
+import type { CallbackData } from '../types';
+export {
+	CallbackData,
+} from '../types';
 export class CallbackUtils {
-	public static composeCallbackData (callbackData: CallbackData): string {
-		return `${callbackData.module}|${callbackData.action ?? ''}|${callbackData.back ? '1' : ''}|${callbackData.meta ?? ''}`;
-	}
-
 	public static parseCallbackData (callbackData: string): CallbackData {
 		const [module, action, back, meta] = callbackData.split('|');
-
 		if (module === undefined || action === undefined || back === undefined || meta === undefined) {
 			throw new Error('Parse callback data error');
 		}
-
 		if (!module.length) {
 			throw new Error('Callback data module is empty');
 		}
-
 		return {
 			module,
 			action,
@@ -23,4 +18,7 @@ export class CallbackUtils {
 			meta,
 		};
 	}
+}
+export function stringifyCallbackData (callbackData: CallbackData): string {
+	return `${callbackData.module}|${callbackData.action ?? ''}|${callbackData.back ? '1' : ''}|${callbackData.meta ?? ''}`;
 }
--- src/core/bot/lib/index.ts
+++ src/core/bot/lib/index.ts
@@ -1,5 +1,5 @@
-export * from './callback-handlers';
-export * from './message-handlers';
-
-export * from './callback-utils';
-export * from './compose-callback-data';
+export {
+	CallbackUtils,
+	stringifyCallbackData,
+	type CallbackData,
+} from './callback-utils';
