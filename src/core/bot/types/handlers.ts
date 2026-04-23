642b59cefba4457c19ba6b8b14e984cc5d3f9120
VladimirBelyaev <dazevvv@gmail.com>
2026-04-24 00:46:16 +0900

    refactor(types): remove unused MessageHandler, CallbackHandler, and HandlerGuard aliases

--- src/core/bot/types/handlers.ts
+++
@@ -1,5 +0,0 @@
-import type { AppContext, CallbackCtx, MessageCtx } from '.';
-
-export type MessageHandler = (ctx: MessageCtx) => Promise<void>;
-export type CallbackHandler = (ctx: CallbackCtx) => Promise<void>;
-export type HandlerGuard<T extends AppContext = AppContext> = (ctx: T) => boolean;
